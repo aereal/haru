aab09d1d79620d0421d649a4a540fb231f23405e
aereal <aereal@aereal.org>
2017-06-06 21:51:31 +0900

    Render photos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,51 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+type Photo = {
+  id: string,
+  largerUrl: string,
+  smallerUrl: string,
+  mediumUrl: string,
+};
+
+class PhotoComponent extends React.PureComponent {
+  props: {
+    photo: Photo,
+  };
+
+  render() {
+    const { photo } = this.props;
+    return (
+      <span itemType="http://schema.org/Photograph">
+        <picture data-photo-id={photo.id}>
+          <source media="(max-width: 640px)" srcSet={photo.smallerUrl} />
+          <source media="(max-width: 1024px)" srcSet={photo.mediumUrl} />
+          <img src={photo.largerUrl} itemProp="image" />
+        </picture>
+      </span>
+    );
+  }
+}
+
+class PhotosListComponent extends React.PureComponent {
+  props: {
+    photos: Photo[],
+  };
+
+  render() {
+    return (
+      <div id="images">
+        {this.props.photos.map((photo, idx) => <PhotoComponent photo={photo} key={idx} />)}
+      </div>
+    );
+  }
+}
+
+const initialPhotos = [
+];
 ReactDOM.render(
-  <p>Webpacked</p>,
+  <PhotosListComponent photos={initialPhotos} />,
   document.querySelector('main')
 );
